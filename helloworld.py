6b3c9d2d63fa4c56808bbc1b99c9eda425dd0010
gushumeng <shumengg@live.unc.edu>
2014-01-16 02:26:49 +0800

    change helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#learning python commands 
+
+print "my name is Shumeng"
+
+print "boys", 25 + 30 / 6
+
+print "girls", 100 - 25 * 3 %4 
+print "let's count the class:"
+
+print 3+2+1-5+4%2 - 1/4 +6
+
